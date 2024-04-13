d2f60855436a23da27ee52940e594c17d916f8b8
Durru Aravind <aravind2000d@gmail.com>
2024-04-13 13:28:07 +0530

    updated filenames

---
+++ Roman_Integer_Conversion/integerToRoman.java
@@ -0,0 +1,27 @@
+package Roman_Integer_Conversion;
+
+import java.util.Scanner;
+
+public class integerToRoman {
+
+    public static String romanToInt(int num) {
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] romanLetters = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num = num - values[i];
+                roman.append(romanLetters[i]);
+            }
+        }
+        return (roman.toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = romanToInt(n);
+        System.out.println(s);
+        sc.close();
+    }
+}
